7c9006c013a6e349f4d7004caea4a467ffbd1d8e
Victor Wesley <victor.infowb@gmail.com>
2026-01-14 13:39:50 -0300

    chore: estrutura inicial projeto

---
+++ src/main/java/com/vcontrola/vcontrola/VcontrolaApplication.java
@@ -0,0 +1,13 @@
+package com.vcontrola.vcontrola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VcontrolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VcontrolaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vcontrola/vcontrola/VcontrolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vcontrola.vcontrola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VcontrolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
