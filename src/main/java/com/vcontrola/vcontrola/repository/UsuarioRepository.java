c7216ca65fd1cb4b49674404af00dc24e2ffe722
Victor Wesley <victor.infowb@gmail.com>
2026-01-14 15:31:44 -0300

    feat: Implementa repositorio JPA para Usuario

--- src/main/java/com/vcontrola/vcontrola/repository/UsuarioRepository.java
+++ src/main/java/com/vcontrola/vcontrola/repository/UsuarioRepository.java
@@ -1,4 +1,14 @@
 package com.vcontrola.vcontrola.repository;
 
-public interface UsuarioRepository {
+
+import com.vcontrola.vcontrola.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
+    Optional<Usuario> findByEmail(String email);
 }
