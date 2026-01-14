173400300dfd455522b61756b294b739bc630688
Victor Wesley <victor.infowb@gmail.com>
2026-01-14 15:41:54 -0300

    feat: Implementa repositorio JPA para Conta

---
+++ src/main/java/com/vcontrola/vcontrola/repository/ContaRepository.java
@@ -0,0 +1,13 @@
+package com.vcontrola.vcontrola.repository;
+
+import com.vcontrola.vcontrola.entity.Conta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface ContaRepository extends JpaRepository<Conta, UUID> {
+    List<Conta> findByUsuarioId(UUID usuarioId);
+}
