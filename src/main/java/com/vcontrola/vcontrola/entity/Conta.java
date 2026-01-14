bd9812288382a7ccd1a135f220c876b07d425c4c
Victor Wesley <victor.infowb@gmail.com>
2026-01-14 14:01:46 -0300

    feat:Implementa entidade conta e validações com Beans validation

---
+++ src/main/java/com/vcontrola/vcontrola/entity/Conta.java
@@ -0,0 +1,35 @@
+package com.vcontrola.vcontrola.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "contas")
+@Data
+public class Conta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotBlank(message = "O nome da conta é obrigatório")
+    private String nome;
+
+    @NotNull(message = "O saldo inicial deve ser informado")
+    @PositiveOrZero(message = "O saldo não pode ser negativo")
+    private BigDecimal saldo;
+
+    @NotNull(message = "O tipo da conta é obrigatório")
+    @Enumerated(EnumType.STRING)
+    private TipoConta tipo;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "usuario_id", nullable = false)
+    @NotNull(message = "A conta deve pertencer a um usuário")
+    private Usuario usuario;
+}
