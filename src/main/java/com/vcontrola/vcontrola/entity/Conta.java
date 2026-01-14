0993aa3251bf15155585376b87295d8dee0d8cf2
Victor Wesley <victor.infowb@gmail.com>
2026-01-14 15:06:33 -0300

    feat:Incluir importação do enum TipoConta na entidade Conta

--- src/main/java/com/vcontrola/vcontrola/entity/Conta.java
+++ src/main/java/com/vcontrola/vcontrola/entity/Conta.java
@@ -1,5 +1,6 @@
 package com.vcontrola.vcontrola.entity;
 
+import com.vcontrola.vcontrola.enums.TipoConta;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
