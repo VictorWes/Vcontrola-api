688a08787cea97b6fdfa0d91e4d06d65f4858d83
Victor Wesley <victor.infowb@gmail.com>
2026-01-15 16:39:38 -0300

    feat: adiciona a anotation de CrossOrigin para liberar a rota para ser utilizada no front

--- src/main/java/com/vcontrola/vcontrola/controller/UsuarioController.java
+++ src/main/java/com/vcontrola/vcontrola/controller/UsuarioController.java
@@ -5,16 +5,14 @@ import com.vcontrola.vcontrola.controller.response.UsuarioResponse;
 import com.vcontrola.vcontrola.service.UsuarioService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 
 @RestController
 @RequestMapping("/usuarios")
+@CrossOrigin(origins = "http://localhost:4200")
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
