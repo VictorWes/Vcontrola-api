eaf231ea147bf4eec537a753447865a004c873be
Victor Wesley <victor.infowb@gmail.com>
2026-02-25 16:40:06 -0300

    fix: Corrige ValorRequest para record

--- src/main/java/com/vcontrola/vcontrola/controller/request/ValorRequest.java
+++ src/main/java/com/vcontrola/vcontrola/controller/request/ValorRequest.java
@@ -1,4 +1,13 @@
 package com.vcontrola.vcontrola.controller.request;
 
-public class ValorRequest {
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+import java.math.BigDecimal;
+
+public record ValorRequest(
+        @NotNull(message = "O valor é obrigatório")
+        @Positive(message = "O valor deve ser positivo")
+        BigDecimal valor
+) {
 }
