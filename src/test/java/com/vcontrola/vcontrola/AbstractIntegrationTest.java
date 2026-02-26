10d7e4561709fc09492abfd7640cd54c0bb1a6eb
Victor Wesley <victor.infowb@gmail.com>
2026-02-26 18:52:52 -0300

    feat: Implementa configuração inicial para funcionamento dos testes de container

--- src/test/java/com/vcontrola/vcontrola/AbstractIntegrationTest.java
+++ src/test/java/com/vcontrola/vcontrola/AbstractIntegrationTest.java
@@ -1,4 +1,17 @@
 package com.vcontrola.vcontrola;
 
-public class AbstractIntegrationTest {
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@Testcontainers
+@ActiveProfiles("test")
+public abstract class AbstractIntegrationTest {
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:latest");
 }
--- src/test/java/com/vcontrola/vcontrola/VcontrolaApplicationTests.java
+++ src/test/java/com/vcontrola/vcontrola/VcontrolaApplicationTests.java
@@ -3,11 +3,15 @@ package com.vcontrola.vcontrola;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
-class VcontrolaApplicationTests {
+class VcontrolaApplicationTests  extends  AbstractIntegrationTest{
 
 	@Test
 	void contextLoads() {
-	}
 
+		System.out.println("Banco esta rodando: " + postgresContainer.isRunning());
+		assertTrue(postgresContainer.isRunning());
+	}
 }
